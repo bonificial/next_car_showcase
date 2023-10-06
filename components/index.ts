3e1c84094d39d27f68a5f79137f8c49dd32645f3
Bonie Mwangi <mboniface107@gmail.com>
2023-10-06 19:19:58 +0300

    Initial Commit

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import CustomButton from "@/components/CustomButton";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import CustomFilter from "@/components/CustomFilter";
+import Searchbar from "@/components/Searchbar";
+import SearchManufacturer from "@/components/SearchManufacturer";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  CustomFilter,
+  SearchManufacturer,
+  Searchbar,
+};
